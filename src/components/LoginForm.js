4a48e004c6ab36e0996732a540db137fe1da550d
Anton Zabulonov <zabulonov.a@gmail.com>
2022-06-19 11:32:24 +0300

    Add styling to basic form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -22,40 +22,41 @@ export default function LoginForm({ login, error }) {
 
 	return (
 		<form onSubmit={submitHandle}>
-			<div className="form-inner"></div>
-			<h2>Login</h2>
-			{error ? <div className="error">{error}</div> : ''}
-			<div className="form-group">
-				<label htmlFor="name">Name: </label>
-				<input
-					type="text"
-					name="name"
-					id="name"
-					onChange={changeForm}
-					value={details.name}
-				/>
+			<div className="form-inner">
+				<h2>Login</h2>
+				{error ? <div className="error">{error}</div> : ''}
+				<div className="form-group">
+					<label htmlFor="name">Name: </label>
+					<input
+						type="text"
+						name="name"
+						id="name"
+						onChange={changeForm}
+						value={details.name}
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="email">Email: </label>
+					<input
+						type="email"
+						name="email"
+						id="email"
+						onChange={changeForm}
+						value={details.email}
+					/>
+				</div>
+				<div className="form-group">
+					<label htmlFor="password">Password: </label>
+					<input
+						type="password"
+						name="password"
+						id="password"
+						onChange={changeForm}
+						value={details.password}
+					/>
+				</div>
+				<button>Login</button>
 			</div>
-			<div className="form-group">
-				<label htmlFor="email">Email: </label>
-				<input
-					type="email"
-					name="email"
-					id="email"
-					onChange={changeForm}
-					value={details.email}
-				/>
-			</div>
-			<div className="form-group">
-				<label htmlFor="password">Password: </label>
-				<input
-					type="password"
-					name="password"
-					id="password"
-					onChange={changeForm}
-					value={details.password}
-				/>
-			</div>
-			<button>Login</button>
 		</form>
 	);
 }
